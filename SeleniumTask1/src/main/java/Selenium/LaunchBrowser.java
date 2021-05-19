d79e7a5b99993682f26e4dbeda83876280fbac38
ranjan-abhii <engineer_abhii@yahoo.com>
2021-05-19 14:18:03 +0530

    Seleniumtask1 using flask

---
+++ SeleniumTask1/src/main/java/Selenium/LaunchBrowser.java
@@ -0,0 +1,17 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com/");
+		
+
+	}
+
+}
